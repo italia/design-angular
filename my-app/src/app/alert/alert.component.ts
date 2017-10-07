f28b0ab35669d29f8361b54d2dbb1dad0044b47d
Nicola Toscan <nicolatoscan99@outlook.it>
2017-10-07 19:17:45 +0200

    Auto stash before rebase of "origin/master"
    Component alert

--- my-app/src/app/alert/alert.component.ts
+++ my-app/src/app/alert/alert.component.ts
@@ -1,17 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'alert',
-    templateUrl: './alert.component.html',
+    templateUrl: './alert.component.html'
 })
 export class AlertComponent {
 
-    public modifier = "warning"
-
-    public title = 'Si è verificato un errore';
-    public description = 'Voluptate ut voluptatem sit earum ipsam sint. Aut unde explicabo eos dolor rerum eum et. Maxime aliquam deserunt. Non officiis eos fugit in perferendis.';
+    @Input() alert: Alert;
 
     constructor() {
     }
 }
+
+export enum EAlertType {
+    error,
+    warning,
+    success,
+    info
+}
+
+export class Alert {
+    public context: AlertContext;
+
+    constructor(
+        public name: string,
+        type: EAlertType,
+        text: string,
+        description: string,
+        icon: string = ""
+    ) {
+        this.context = new AlertContext(type, text, description, icon);
+    }
+}
+
+export class AlertContext {
+    public modifier: string;
+
+    constructor(
+        public type: EAlertType,
+        public text: string,
+        public description: string,
+        public icon: string
+    ) {
+
+        switch (this.type) {
+            case EAlertType.error:
+                this.modifier = "error"
+                break;
+            case EAlertType.warning:
+                this.modifier = "warning"
+                break;
+            case EAlertType.info:
+                this.modifier = "info"
+                break;
+            case EAlertType.success:
+                this.modifier = "success"
+                break;
+            default:
+                this.modifier = "";
+                break;
+        }
+    }
+}
+
 
--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
 import { ConstantService } from './constant.service';
+import { Alert, EAlertType } from "./alert/alert.component";
 
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-    public title = 'app';
-    public description = "prova 1";
+
+    public Alerts: Alert[] = [];
 
     constructor() {
+        this.Alerts.push(new Alert("warning", EAlertType.error, "Warning 1", "Descrizione 1"));
+        this.Alerts.push(new Alert("warning", EAlertType.warning, "Warning 1", "Descrizione 1"));
+        this.Alerts.push(new Alert("warning", EAlertType.info, "Warning 1", "Descrizione 1"));
+        this.Alerts.push(new Alert("warning", EAlertType.success, "Warning 1", "Descrizione 1"));
     }
 }
