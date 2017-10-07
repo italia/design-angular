ca6603a2c34315cc519bbd672eb98c1ba886fb32
Francesco <francesco42.dalbello@gmail.com>
2017-10-08 01:12:41 +0200

    Modifiche nomi variabili

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ConstantService } from './constant.service';
 import { Alert, EAlertType } from "./alert/alert.component";
 import { Accordion } from './accordion/accordion.component';
+import { Breadcrumb } from './breadcrumb/breadcrumb.component';
 
 
 @Component({
@@ -10,23 +11,32 @@ import { Accordion } from './accordion/accordion.component';
 })
 export class AppComponent {
 
-    public alerts: Alert[] = [];
-    public accordions: Accordion[] = [];
+    public accordionsTest: Accordion[] = [];
+    public alertsTest: Alert[] = [];
+    public breadcrumbsTest: Breadcrumb[] = [];
     
 
     constructor() {
-        this.alerts.push(new Alert("warning", EAlertType.error, "Warning 1", "Descrizione 1"));
-        this.alerts.push(new Alert("warning", EAlertType.warning, "Warning 1", "Descrizione 1"));
-        this.alerts.push(new Alert("warning", EAlertType.info, "Warning 1", "Descrizione 1"));
-        this.alerts.push(new Alert("warning", EAlertType.success, "Warning 1", "Descrizione 1"));
-
-
-        this.accordions = [
+        this.accordionsTest = [
             new Accordion ("aa","aa"),
             new Accordion ("ba","ba"),
             new Accordion ("ca","ca"),
             new Accordion ("da","da"),
             new Accordion ("ea","ea"),
+        ]
+
+        this.alertsTest = [
+            new Alert("warning", EAlertType.error, "Warning 1", "Descrizione 1"),
+            new Alert("warning", EAlertType.warning, "Warning 1", "Descrizione 1"),
+            new Alert("warning", EAlertType.info, "Warning 1", "Descrizione 1"),
+            new Alert("warning", EAlertType.success, "Warning 1", "Descrizione 1")
+        ]
+        
+        this.breadcrumbsTest = [
+            new Breadcrumb("http://www.barsanti.gov.it", "hh"),
+            new Breadcrumb("http://www.barsanti.gov.it", "devCoseBelle"),
+            new Breadcrumb("http://www.barsanti.gov.it", "arcobaleni"),
+            new Breadcrumb("http://www.barsanti.gov.it", "hh")
         ]
         
     }
