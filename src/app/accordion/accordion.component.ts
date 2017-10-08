9958fffe59bae691b1502babcf0e9be0301b4d1a
Andrea Collet <andrea@fluidforfun.net>
2017-10-08 09:50:37 +0200

    Initialized accordion

--- src/app/accordion/accordion.component.ts
+++ src/app/accordion/accordion.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ElementRef } from '@angular/core';
+import Fraccordion from 'fr-accordion';
 
 @Component({
     selector: 'accordion',
@@ -8,8 +9,23 @@ import { Component, Input } from '@angular/core';
 export class AccordionComponent {
 
     @Input() accordionsList: Accordion[] = [];
-    
-    constructor() {
+
+    constructor(private el: ElementRef) {
+    }
+
+    ngAfterViewInit() {
+        var myAccordion = Fraccordion({
+            // String - Use header id on element to tie each accordion panel to its header - see panelIdPrefix
+            headerIdPrefix: 'accordion-header',
+            // String - Use panel id on element to tie each accordion header to its panel - see headerIdPrefix
+            panelIdPrefix: 'accordion-panel',
+            // Boolean - If set to false, all accordion panels will be closed on init()
+            firstPanelsOpenByDefault: false,
+            // Boolean - If set to false, each accordion instance will only allow a single panel to be open at a time
+            multiselectable: true,
+            // String - Class name that will be added to the selector when the component has been initialised
+            readyClass: 'fr-accordion--is-ready'
+        });
     }
 }
 
